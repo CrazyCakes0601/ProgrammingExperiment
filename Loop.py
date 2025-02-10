d9df174d20abdf9fea5f379f06823ee409de4f33
Naomi Davis <crazycakes0601@gmail.com>
2025-02-10 11:11:21 -0500

    1st Pattern

---
+++ Loop.py
@@ -0,0 +1,11 @@
+from turtle import *
+delay(0)
+for steps in range(100):
+    for c in ( 'gray' , 'tan' , 'black' ,):
+        color(c)
+        forward(steps)
+        right(30)
+        # gray + tan + black -  green + purple + blue - cyan + purple + hot pink
+        # hot pink + yellow + cyan - navy + blue + cyan
+hideturtle()
+done()        
